cf86c3816e17b88e18b1f9f064f95106a1db7aad
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-31 21:43:04 +0530

    ggg ---h

--- server/app.ts
+++ server/app.ts
@@ -28,7 +28,7 @@ app.use(fileUpload({
 }));
 app.use(
     cors({
-        origin: ["https://www.solvit.live"],
+        origin: ["https://solvit-test-deploy.vercel.app"],
         methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
