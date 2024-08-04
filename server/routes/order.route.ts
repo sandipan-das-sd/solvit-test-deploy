c58573831d0387b18d31fbeaeafb3efd82eb3c8f
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-08-04 22:11:19 +0530

    updated

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -18,7 +18,7 @@ orderRouter.get(
 );
 
 orderRouter.get("/payment/stripepublishablekey", sendStripePublishableKey);
-orderRouter.get("/payment_intents/stripepublishablekey/confirm", sendStripePublishableKey);// i have added
+
 orderRouter.post("/payment", isAutheticated, newPayment);
-orderRouter.post("/payment_intents/stripepublishablekey/confirm", isAutheticated, newPayment, sendStripePublishableKey);//i have added
+
 export default orderRouter;
