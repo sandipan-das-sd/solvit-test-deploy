e6aeae34bb5ee1a9703558f14818a7041499eba9
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-08-04 23:58:04 +0530

    updated

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,40 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["res.cloudinary.com", "solvit-client.vercel.app", "solvit-test-deploy.vercel.app"],
-//   },
-//   experimental: {
-//     reactRoot: true,
-//     suppressHydrationWarning: true,
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com", // Adjusted to match the Facebook domains
-      },
-      {
-        protocol: "https",
-        hostname: "**", // Allow images from any domain (use with caution)
-      },
-    ],
-    domains: [
-      "res.cloudinary.com",
-      "solvit-client.vercel.app",
-      "solvit-test-deploy.vercel.app",
-    ],
+    domains: ["res.cloudinary.com", "solvit-client.vercel.app", "solvit-test-deploy.vercel.app"],
   },
   experimental: {
     reactRoot: true,
     suppressHydrationWarning: true,
-  },
-  env: {
-    ACCESS_TOKEN: process.env.ACCESS_TOKEN_SECRET,
   },
 };
 
