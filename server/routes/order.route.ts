78e937d71e12bf5aa03460e147d207438cdca382
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-08-04 21:55:16 +0530

    updated

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -18,7 +18,7 @@ orderRouter.get(
 );
 
 orderRouter.get("/payment/stripepublishablekey", sendStripePublishableKey);
-
+orderRouter.get("/payment_intents/stripepublishablekey/confirm", sendStripePublishableKey);
 orderRouter.post("/payment", isAutheticated, newPayment);
-
+orderRouter.post("/payment_intents/stripepublishablekey/confirm", isAutheticated, newPayment, sendStripePublishableKey);
 export default orderRouter;
