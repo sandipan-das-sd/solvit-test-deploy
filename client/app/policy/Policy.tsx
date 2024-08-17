6d76bb0c786c024b812b26b306e4ec2aa7435e97
sandipan-das-sd <sd901656@gmail.com>
2024-08-17 22:50:42 +0530

    policy updated

--- client/app/policy/Policy.tsx
+++ client/app/policy/Policy.tsx
@@ -1,3 +1,82 @@
+// import React from "react";
+// import { styles } from "../styles/style";
+
+// type Props = {};
+
+// const Policy = (props: Props) => {
+//   return (
+//     <div>
+//       <div className={"w-[95%] 800px:w-[92%] m-auto py-2 text-black dark:text-white px-3"}>
+//         <h1 className={`${styles.title} !text-start pt-2`}>
+//           Platform Terms and Condition
+//         </h1>
+//       <ul style={{ listStyle: "unset", marginLeft: "15px" }}>
+//       <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores!
+//         </p>
+//         <br />
+//         <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+//         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores!
+//         </p>
+//         <br />
+//         <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+//         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores!
+//         </p>
+//         <br />
+//         <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+//         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores!
+//         </p>
+//         <br />
+//         <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+//         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores!
+//         </p>
+//         <br />
+//         <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+//         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+//           blanditiis architecto quasi impedit in dicta nisi, asperiores
+//           voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
+//           accusantium quod numquam dolores!
+//         </p>
+//       </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Policy;
 import React from "react";
 import { styles } from "../styles/style";
 
@@ -8,69 +87,66 @@ const Policy = (props: Props) => {
     <div>
       <div className={"w-[95%] 800px:w-[92%] m-auto py-2 text-black dark:text-white px-3"}>
         <h1 className={`${styles.title} !text-start pt-2`}>
-          Platform Terms and Condition
+          Platform Terms and Conditions
         </h1>
-      <ul style={{ listStyle: "unset", marginLeft: "15px" }}>
-      <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores!
-        </p>
-        <br />
-        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores!
-        </p>
-        <br />
-        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores!
-        </p>
-        <br />
-        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores!
-        </p>
-        <br />
-        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores!
-        </p>
-        <br />
-        <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-          blanditiis architecto quasi impedit in dicta nisi, asperiores
-          voluptatum eos alias facilis assumenda ex beatae, culpa dignissimos
-          accusantium quod numquam dolores!
-        </p>
-      </ul>
+        <ul style={{ listStyle: "unset", marginLeft: "15px" }}>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            Our website provides Users with access to compiled educational information and related sources. Such information is provided on an As Is basis and We assume no liability for the accuracy or completeness or use or non-obsolescence of such information. We shall not be liable to update or ensure continuity of such information contained on the Website. We would not be responsible for any errors, which might appear in such information, which is compiled from third party sources or for any unavailability of such information. That any modification or change in facts may occur and shall be the responsibility of the third parties involved to provide necessary methods or answers.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            <strong>USER TERMS & CONDITIONS</strong><br />
+            One user upon acceptance and agreeing to the terms and conditions herein below shall accept and be bound with the terms and conditions as set forth and any violation of the same shall be equivocally treated as breach of contract herein.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            That the user shall by validating his phone number and email and shall register within the website to avail the necessary educational information and materials available, upon paying the fees as admissible through the Company.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            That the User upon non satisfaction or other clauses may ask for any refund and that no refund system is available herein. That no compensation or money back is available with the platform and that no such claim shall be entertained.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            Users cannot log in as educators or third parties or other fake identity or multiple accounts and cannot engage in any business activities using the platform.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            The PYQ Platform will not call any user and ask for an OTP. Users are advised not to share their personal details with anyone on the platform. That as per the mechanism of software as held by the company the user ID is mapped with the phone number but not the phone. So, if a user loses their phone but still recovers the SIM, they can log in with the same ID. If unable to recover the SIM, the user has to register again.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            That the user once creates the website cannot share his or her ID with any other users. If found otherwise, legal action will be taken. That all information, content, material, trademarks, services marks, trade names, and trade secrets including but not limited to the software, text, images, graphics, video, script and audio, contained in the Application, Website, Services and products are proprietary property of the Company (“Proprietary Information”). That if any user or otherwise is found monetizing upon the contents and materials within the Website then the same shall be held punishable under the breach of contract vide Indian Contract Act,1872 and relevant criminal provisions shall apply.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            No Proprietary Information may be copied, downloaded, reproduced, modified, republished, uploaded, posted, transmitted or distributed in any way without obtaining prior written permission from the Company and nothing on this Application or Website or Services shall be or products deemed to confer a license of or any other right, interest or title to or in any of the intellectual property rights belonging to the Company, to the User. Users are strictly prohibited from creating and publicly sharing any videos generated using AI technology that may cause harm, misinformation, or any form of disturbance. Violations may lead to legal consequences. Recording of the videos is not permissible on this platform, and recording or causing duplicate profiles to circulate the exclusive contents in a public domain shall be treated as breach of contract and appropriate damages and compensation shall be claimed.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            You may own the medium on which the information, content or materials resides, but the Company shall at all times retain full and complete title to the information, content or materials and all intellectual property rights inserted by the Company on such medium and shall be as a binding contract. Certain contents on the Website may belong to third parties. Such contents have been reproduced after taking prior consent from said party and all rights relating to such content will remain with such third party. Further, you recognize and acknowledge that the ownership of all trademarks, copyright, logos, service marks and other intellectual property owned by any third party or of the Company shall continue to vest with such party and You are not permitted to use the same without the consent of the respective Company.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            <strong>EDUCATOR TERMS & CONDITIONS:</strong><br />
+            Once the educator enters into the agreement with the PYQ Platform then they are bound by the rules and agreement as made between the parties. That the educational material prepared in any medium/by script or by video is and shall be made by the educator upon consent and acceptance with the terms as laid by PYQ Platform. Once educator uploads their content to our platform, then the PYQ Platform team has the right to use that video for any future reference. That all videos and materials curated and created are as per the remuneration paid to educator to conclusively shoot and send the videos to the PYQ Platform. That educator shall be bound the provisions of Copyright Act,1872 and the relevant amendment provisions.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            The PYQ platform being the sole owner and proprietor of the contents shall have the absolute right to remove or delete videos of educators if the content is not relevant or good.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            The Educators shall be bound to share their original videos; they cannot copy content from other websites, for example, using content from platforms like "XYZ Learning/Academy/Platform or any app."
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            That the Educators cannot use any rough language/abusive language/behaviour dressing sense or engage in inappropriate behaviour with the users/ students while clearing doubts.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            Educators and the PYQ platform team will mutually decide on the pricing for the content. Educators cannot charge exorbitant prices from users. They shall be bound by the monetary aspects as per the agreement.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            If any educators want to upload their content, they have to request it first in the app. If the request is accepted by the PYQ Platform, then they can upload content. All relevant instructions will be sent by the PYQ platform itself.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            That the educators cannot engage in business using our domain and server, reveal any relevant security data, or engage in any business activities that involve the platform. That the users cannot be engaged to any private tuition or education through one-to-one source which is violative of the contract and the same if found shall be punishable as breach of contract. Educators cannot log in as users and cannot sell courses intended for user use or make any miscreance use of the same.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            In no event shall the Company, its officers, directors, employees, partners or agents be liable to You or any third party for any special, incidental, indirect, consequential or punitive damages whatsoever, including those resulting from loss of use, data or profits, whether or not foreseeable or whether or not Company has been advised of the possibility of such damages, or based on any theory of liability, including breach of contract or warranty, negligence or other tortuous action, or any other claim arising out of or in connection with Your use of or access to the Application, Website or Services.
+          </p>
+          <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
+            The limitations and exclusions in this section apply to the maximum extent permitted by applicable law.
+          </p>
+        </ul>
       </div>
     </div>
   );
