60b931145d0756e60a8846f8829754f3372ba918
sandipan-das-sd <sd901656@gmail.com>
2024-08-30 10:00:01 +0530

    server updated notifcation

--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 import { getNotifications, updateNotification } from "../controllers/notification.controller";
+import { adminUserNotification,sendNotificationToAllUsers,getAllNotifications,getUserNotifications,markNotificationAsRead,getNotificationCount,clearNotifications } from "../controllers/admin_user_notification.controller";
+import { send } from "process";
 const notificationRoute = express.Router();
 
 notificationRoute.get(
@@ -10,5 +12,46 @@ notificationRoute.get(
   getNotifications
 );
 notificationRoute.put("/update-notification/:id", isAutheticated, authorizeRoles("admin"), updateNotification);
+
+// Route to send notification to a specific user
+notificationRoute.post(
+  '/send-notification',
+  isAutheticated,
+  authorizeRoles('admin'),
+  adminUserNotification
+);
+
+// Route to send notification to all users
+notificationRoute.post(
+  '/send-notification-all',
+  isAutheticated,
+  authorizeRoles('admin'),
+  sendNotificationToAllUsers
+);
+
+// New route to get notifications for a specific user
+notificationRoute.get(
+  '/user-notifications/:userId',
+  isAutheticated,
+  getUserNotifications
+);
+
+// New route to get all notifications (for admin)
+notificationRoute.get(
+  '/all-notifications',
+  isAutheticated,
+  authorizeRoles('admin'),
+  getAllNotifications
+);
+
+//mark notiifaction
+notificationRoute.put(
+  '/mark-notification-read/:notificationId',
+  isAutheticated,
+  markNotificationAsRead
+);
+
+notificationRoute.delete('/clear-notifications/:userId', isAutheticated, clearNotifications);
+notificationRoute.get('/notification-count/:userId', isAutheticated, getNotificationCount);
 
 export default notificationRoute;
